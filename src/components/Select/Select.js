e9ab758a757cb1abf62ff2964c49543069a41841
Jitendra Patel <jitendra.patel@kantar.com>
2022-11-23 00:56:06 +0530

    Select Component completed.

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -1,18 +1,66 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-import { COLORS } from '../../constants';
-import Icon from '../Icon';
-import { getDisplayedValue } from './Select.helpers';
+import { COLORS } from "../../constants";
+import Icon from "../Icon";
+import { getDisplayedValue } from "./Select.helpers";
 
 const Select = ({ label, value, onChange, children }) => {
   const displayedValue = getDisplayedValue(value, children);
 
   return (
-    <select value={value} onChange={onChange}>
-      {children}
-    </select>
+    <SelectWrapper>
+      <SelectTag value={value} onChange={onChange}>
+        {children}
+      </SelectTag>
+      <SelectFront>
+        {displayedValue}
+        <IconWrapper>
+          <Icon id="chevron-down" size="24"></Icon>
+        </IconWrapper>
+      </SelectFront>
+    </SelectWrapper>
   );
 };
+
+const SelectWrapper = styled.div`
+  position: relative;
+  width: max-content;
+`;
+
+const SelectTag = styled.select`
+  z-index: 1;
+  opacity: 0;
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  width: 100%;
+  height: 100%;
+`;
+
+const SelectFront = styled.div`
+  background: ${COLORS.transparentGray15};
+  color: ${COLORS.gray700};
+  padding: 12px 16px;
+  padding-right: 40px;
+  border-radius: 8px;
+  font-size: 1rem;
+  ${SelectTag}:focus + & {
+    outline: 1px solid #212121;
+  }
+  ${SelectTag}:hover + & {
+    color: ${COLORS.black};
+  }
+`;
+
+const IconWrapper = styled.div`
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  right: 8px;
+  margin: auto;
+  width: 24px;
+  height: 24px;
+`;
 
 export default Select;
